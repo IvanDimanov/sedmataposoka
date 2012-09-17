ff83f2059adbbdb1afdae1edd373bc5116339182
IvanDimanov <IvanDimanov1@gmail.com>
2012-09-17 10:28:30 +0300

    Installing CodeIgniter

---
+++ test_code/client/index.php
@@ -0,0 +1,25 @@
+<?php
+  // Test PHP index file
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title><?php echo $title ?></title>
+  </head>
+  <body>
+    
+    <h1>That's our first PHP index page</h1>
+    
+    <h2>New header</h2>
+    
+    
+  </body>
+</html>
+
+
+
+content
+caption | EN | BG
+------------------
+title | Seven... | Седем ...
+
