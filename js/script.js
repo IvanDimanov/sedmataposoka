fa197002baf793fa4e70d9466e145d78848e1260
IvanDimanov <IvanDimanov1@gmail.com>
2012-10-09 22:23:13 +0300

    JS problem

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 /* This file contains common JS functionalities for All project HTML files */
+
+(function () {
+  
+  // Shorter debug function
+  function log(msg) {
+    console.log(msg);
+  }
+  
+  var n1 = 1;
+  
+  function sum() {
+    n1++;
+  }
+  
+  log(n1);
+})()
