6c5b7449882d2cdb82ed273f8b352b1b6629a1d6
fitil4e <teodorapshv@gmail.com>
2013-02-28 18:06:31 +0200

    view templates merged in templateLayout Modified category, event due to view merging adsModel modified to implement limitation up to 7 ads

--- application/controllers/category.php
+++ application/controllers/category.php
@@ -13,13 +13,45 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class category extends CI_Controller {
     //put your code here
+    public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('toughtModel');
+                $this->load->model('partnerModel');
+                $this->load->model('adsModel');
+                $this->load->model('categoryModel');
+                $this->load->model('subcategoryModel');
+                $this->load->model('categoryModel');
+	}
+        
     function index($catId = 1)
     {
         //todo set language
         $language = 'en';
-        $this->load->model('categoryModel');
+        
         $data['categoryInfo'] = $this->categoryModel->getCategoryInfo($catId, $language);        
+        
+        
+        //TODO move in helper function
+        
+        $this->load->view('templates/header');
+        //get banner for today type=1, limi = 7
+        $data['banners'] = $this->adsModel->getAds( null,1,7,$language);
+        //get adds for today type = 2, limit = 2
+        $data['ads'] = $this->adsModel->getAds( null,2,2,$language);
+        echo 'ADS </br>';
+        $data['partners'] = $this->partnerModel->getPartners($language);
+        $data['tought'] = $this->toughtModel->getTought(null,1,$language);
+        $data['categories'] = $this->categoryModel->
+                getAllCategoriesName($language);
+        $data['subcategories'] = $this->subcategoryModel->
+                getSubcategoriesForCategory($catId = null,$language);
+        
+        
+        $this->load->view('templates/templateLayout',$data);
+        
         $this->load->view('categoryView',$data);
+        $this->load->view('templates/footer');
     }
     
     
