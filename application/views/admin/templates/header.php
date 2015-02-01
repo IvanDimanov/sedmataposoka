50b1b0910d6fa47a00e5b520f120d972a460cf9e
cucka <tzvete_dim@abv.bg>
2015-02-02 00:10:32 +0200

    + admin: header and footer for templates + admin: home page + admin: log out user ~ admin: log in - show captcha

---
+++ application/views/admin/templates/header.php
@@ -0,0 +1,26 @@
+﻿<?php
+// A template file used for loading all HTML page header elements
+
+$this->load->helper('url');
+
+/*Detect any requests coming as URL strings*/
+$query = $_SERVER['QUERY_STRING'] ? '?'.$_SERVER['QUERY_STRING'] : '';
+
+/*Secure at least language URL var*/
+$uri_string = $this->uri->uri_string();
+$uri_string = $uri_string ? $uri_string : $language;
+
+/*Combine the final URL link & remove the not needed 'index.php' location*/
+$full_url = $this->config->site_url().'/'.$uri_string.$query;
+$full_url = str_replace('/index.php', '', $full_url);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- TODO: add page title for different pages -->
+    <title>Admin</title>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/admin/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/admin/admin.css">
+</head>
+<body>
--- js/admin/admin.js
+++ js/admin/admin.js
@@ -3,29 +3,53 @@ $( document ).ready(function() {
 
 	/* Log in user */
 	$('#form-admin-login').on('submit', function(event){
+
 		event.preventDefault();
+
 		var $login_form  = $('#form-admin-login'),
 			name         = $login_form.find('#inputName').val(),
 			pass         = $login_form.find('#inputPassword').val(),
 			/* crypted pass */
-			pass_cripted = CryptoJS.SHA3(pass).toString();
-			
+			pass_cripted = CryptoJS.SHA3(pass, { outputLength: 256 }).toString(),
+			url			 = window.location.origin + '/sedmataposoka/rest_api/login';
+
 		  $.ajax({
 			type: 'POST',
-			/* TODO: take current URL */
-			url : 'http://localhost/sedmataposoka/rest_api/login',
+			url : url,
 			data: {
 			  "name"    : name,
 			  "password": pass_cripted
 			}
 		  })
 		  .done(function (statusText, status, jqXHR) {
-			  console.log('success');
+			window.location.reload();
 		  })
 		  .fail(function (jqXHR, status, statusText) {
-			console.log('error');
+			response_data = JSON.parse(jqXHR.responseText);
+			if (response_data.captcha_required) {
+				$('.captcha').show();
+			}
 		  });
-		  
+
 	  });
 	  
+
+	/* Log in user */
+	$('#log_out').on('click', function(event){
+
+		var url = window.location.origin + '/sedmataposoka/rest_api/login';
+
+		$.ajax({
+			type: 'DELETE',
+			url : url,
+			data: {}
+		})
+		.done(function (statusText, status, jqXHR) {
+		 	window.location.reload();
+		})
+		.fail(function (jqXHR, status, statusText) {
+			// TODO: notify user for a problem
+		});
+		  
+	  });	  
 });
