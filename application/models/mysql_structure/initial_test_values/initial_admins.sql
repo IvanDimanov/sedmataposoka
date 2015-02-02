de05af1618ddb46cf502601cddbf360074d65925
IvanDimanov <IvanDimanov1@gmail.com>
2015-02-02 18:18:59 +0200

    ! set correct sha3 hashed password for the main user

--- application/models/mysql_structure/initial_test_values/initial_admins.sql
+++ application/models/mysql_structure/initial_test_values/initial_admins.sql
@@ -1,14 +1,14 @@
 /*Basic and personal users for the administration panel*/
 insert into admin
 (name         , type         , password                                  , salt              , is_active) values
-('7Admin'     , 'super_admin', '6828d28372d2b369efefbbf75dcb58a49ba0df67', '0b5745acf1e6c9'  , 1        ), /*Password: 6c730ae5d030587ee60254aa4d0eb4174f9e8b4fc4a8cf59e5388cba396c77af = sha3('7@posoka')*/
+('7Admin'     , 'super_admin', '2f3008603dedf1f854f8e3bf36b3b527a3fd8a01', '0b5745acf1e6c9'  , 1        ), /*Password: c0380be174a91aeae153dc9115d8cdb8e468e97341bb04234f51b2b39be0ad89 = sha3('7@posoka')*/
 ('IvanDimanov', 'super_admin', '6c5073a6bf37efa0c9fae8f728e5efc4e7957ec8', '792b4b267c2da11e', 1        ); /*Password: da0d5b797b7c31d56ed318b54213e979adc9b9e12d25f1569ff7ded601338671 = sha3('dimanov$498')*/
 
 
 /*
 Common password + salt query:
-  SELECT * FROM admin where password = sha( concat("6c730ae5d030587ee60254aa4d0eb4174f9e8b4fc4a8cf59e5388cba396c77af", salt))
+  SELECT * FROM admin where password = sha( concat("c0380be174a91aeae153dc9115d8cdb8e468e97341bb04234f51b2b39be0ad89", salt))
 
 where
-  6c730ae5d030587ee60254aa4d0eb4174f9e8b4fc4a8cf59e5388cba396c77af = sha3('7@posoka')
+  c0380be174a91aeae153dc9115d8cdb8e468e97341bb04234f51b2b39be0ad89 = sha3('7@posoka')
 */
